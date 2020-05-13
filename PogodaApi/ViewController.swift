e79d28ccded630bc7428375f04add3f79189f5a5
Viktor <smirn04@yandex.ru>
2020-05-13 20:12:21 +0300

    Initial Commit

---
+++ PogodaApi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PogodaApi
+//
+//  Created by Viktor Smirnov on 13.05.2020.
+//  Copyright © 2020 Viktor Smirnov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
